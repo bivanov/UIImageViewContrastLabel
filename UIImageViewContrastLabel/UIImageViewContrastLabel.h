ca6d02597e1c9df9c2ea88aedb1c6519bc554a26
Bohdan Ivanov <bogdanivanov@live.com>
2017-08-19 17:12:33 +0300

    Initial Commit

---
+++ UIImageViewContrastLabel/UIImageViewContrastLabel.h
@@ -0,0 +1,19 @@
+//
+//  UIImageViewContrastLabel.h
+//  UIImageViewContrastLabel
+//
+//  Created by Bohdan Ivanov on 19.08.17.
+//  Copyright © 2017 bivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageViewContrastLabel.
+FOUNDATION_EXPORT double UIImageViewContrastLabelVersionNumber;
+
+//! Project version string for UIImageViewContrastLabel.
+FOUNDATION_EXPORT const unsigned char UIImageViewContrastLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageViewContrastLabel/PublicHeader.h>
+
+

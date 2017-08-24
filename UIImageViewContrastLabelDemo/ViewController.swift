5b93d940fb6183d822205e3b2ccf92e562b263e9
Bohdan Ivanov <bogdanivanov@live.com>
2017-08-25 02:12:52 +0300

    CALayer subclass; fixed bug with font

--- UIImageViewContrastLabelDemo/ViewController.swift
+++ UIImageViewContrastLabelDemo/ViewController.swift
@@ -13,23 +13,36 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     
+    var constrastLayer: CAContrastLabelLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imageView.addContrastLabel(text: "Hello world!", font: UIFont(name: "Courier", size: 15.0)!)
+        self.constrastLayer = imageView.addContrastLabel(text: "Hello world!",
+                                               font: UIFont(name: "Courier", size: 30.0)!)
         
-/**
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { 
-            self.imageView.removeContrastLabel()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [unowned self] in
+            self.animateContrastLabel()
+//            self.constrastLayer.textPosition = CGPoint(x: 0.5, y: 0.5)
         }
-*/
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func animateContrastLabel() {
+        let animation = CABasicAnimation(keyPath: #keyPath(CATextLayer.position))
+        animation.duration = 4.0
+        animation.repeatCount = 1.0
+        animation.fromValue = self.constrastLayer.textLayer.position
+        animation.toValue = CGPoint(x: self.constrastLayer.textLayer.position.x,
+                                    y: 300)
+        
+        self.constrastLayer.textLayer.add(animation, forKey: "position")
+    }
 
 }
 
